142a88e4d79907aabe966303b4ccf57be8cc060a
JoeRichard7 <143209931+JoeRichard7@users.noreply.github.com>
2024-04-11 12:04:20 +0530

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react"
+    ],
+    "globals": {
+        "window": true,
+        "document": true
+    },
+    "rules": {
+        "no-undef": 0,
+        "no-unused-vars": 0,
+        "react/prop-types": 0,
+        "react/jsx-no-bind": 0,
+        "react/jsx-filename-extension": 0,
+    }
+};
